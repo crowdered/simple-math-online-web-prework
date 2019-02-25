ea1b953d40e2a86f0d80ed37236fb8cae69ac042
Mark Crowder <crowdered@gmail.com>
2019-02-25 14:37:42 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,28 +1,28 @@
 def addition(num1, num2)
-  puts 5 + 4
+  5 + 4
 end
 
 def subtraction(num1, num2)
-  puts 10 - 5
+  10 - 5
 end
 
 def division(num1, num2)
-  puts 50 / 2
+  50 / 2
 end
 
 def multiplication(num1, num2)
-  puts 4 * 30
+  4 * 30
 end
 
 def modulo(num1, num2)
-  puts 34 % 5
+  34 % 5
 end
 
 def square_root(num)
-  puts Math.sqrt(81)
+  Math.sqrt(81)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  puts 7 + (43 * 23) / 83
+  7 + (43 * 23) / 83
   #Hint:  __ + (( __ * __ ) / __ )
 end
